20efb44fa524e2ff6ccbe39e3eb343811414ba45
Carlos Blandino <cablandino66@gmail.com>
2021-04-27 16:38:07 -0700

    making corrections to data files

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const { PORT = 5004} = process.env ;
 const path = require("path");
- // const app = require(path.resolve(
- //     `${process.env.SOLUTION_PATH || ""}`,
- //     "src/app"
- // ));
+ const app = require(path.resolve(
+     `${process.env.SOLUTION_PATH || ""}`,
+     "src/app"
+ ));
 const app = require("./app");
 const listener = () => console.log(`Listening on PORT ${PORT}`);
 app.listen(PORT,listener);
